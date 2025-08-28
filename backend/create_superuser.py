9169dcd00a3972088c2d4a5679f8666f93475e5c
z-fourteen <zgh1016@mail.usct.edu.cn>
2025-08-28 18:12:53 +0800

    修复超级用户创建脚本

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -1,13 +1,11 @@
+import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+password = os.environ.get('SUPERUSER_PASSWORD')
+if not password:
+    raise ValueError("SUPERUSER_PASSWORD environment variable is not set.")
 
-username = "z-fourteen"
-password = "986051zgh"
-email = "zgh1016@mail.ustc.edu.cn"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created.")
-else:
-    print("Superuser already exists.")
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('z-fourteen', 'zgh1016@mail.ustc.edu.cn', password)
+    print("Superuser created successfully!")
